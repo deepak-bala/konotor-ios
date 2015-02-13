2d01c40f8cab14024e898c723891a2d59d796302
Srikrishnan Ganesan <srikrishnan.g@gmail.com>
2015-02-13 14:09:25 +0530

    Force main thread where needed
    Force UI updates and core data changes on main thread

--- Konotor/KonotorInterfaceFiles/KonotorFeedbackScreen.m
+++ Konotor/KonotorInterfaceFiles/KonotorFeedbackScreen.m
@@ -53,43 +53,46 @@ static KonotorFeedbackScreen* konotorFeedbackScreen=nil;
     if(fbScreen.conversationViewController!=nil)
         return NO;
     else{
-        konotorFeedbackScreen.conversationViewController=[[KonotorFeedbackScreenViewController alloc] initWithNibName:@"KonotorFeedbackScreenViewController" bundle:nil];
-        if(KONOTOR_PUSH_ON_NAVIGATIONCONTROLLER){
-            if([[[[[UIApplication sharedApplication] delegate] window] rootViewController] isMemberOfClass:[UINavigationController class]]){
-                [(UINavigationController*)[[[[UIApplication sharedApplication] delegate] window] rootViewController] pushViewController:konotorFeedbackScreen.conversationViewController animated:YES];
-                
-                konotorFeedbackScreen.conversationViewController=nil;
-                return YES;
+        
+        dispatch_async(dispatch_get_main_queue(), ^{
+            konotorFeedbackScreen.conversationViewController=[[KonotorFeedbackScreenViewController alloc] initWithNibName:@"KonotorFeedbackScreenViewController" bundle:nil];
+            if(KONOTOR_PUSH_ON_NAVIGATIONCONTROLLER){
+                if([[[[[UIApplication sharedApplication] delegate] window] rootViewController] isMemberOfClass:[UINavigationController class]]){
+                    [(UINavigationController*)[[[[UIApplication sharedApplication] delegate] window] rootViewController] pushViewController:konotorFeedbackScreen.conversationViewController animated:YES];
+                    
+                    konotorFeedbackScreen.conversationViewController=nil;
+                    return;
+                }
             }
-        }
-        [konotorFeedbackScreen.conversationViewController setModalPresentationStyle:UIModalPresentationFullScreen];
-        [konotorFeedbackScreen.conversationViewController setModalTransitionStyle:UIModalTransitionStyleCrossDissolve];
-        
-        if([KonotorUtility KonotorIsInterfaceLandscape: (konotorFeedbackScreen.conversationViewController)]){
-        konotorFeedbackScreen.window=[[UIWindow alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height)];
-        }
-        else{
-            konotorFeedbackScreen.window=[[UIWindow alloc] initWithFrame:[[[UIApplication sharedApplication] delegate] window].bounds];
-        }
-
-        [konotorFeedbackScreen.window setBackgroundColor:[UIColor colorWithRed:0.2 green:0.2 blue:0.2 alpha:0.7]];
-        [konotorFeedbackScreen.window setRootViewController:konotorFeedbackScreen.conversationViewController];
-        
-        UIViewController *rootViewController=[[[[UIApplication sharedApplication] delegate] window] rootViewController];
-        if([rootViewController isKindOfClass:[UINavigationController class]])
-            rootViewController=[((UINavigationController*)rootViewController) topViewController];
-        UIViewController *presentedViewController=[rootViewController presentedViewController];
-        while(presentedViewController!=nil){
-            rootViewController=presentedViewController;
-            presentedViewController=[rootViewController presentedViewController];
-        }
-        
-        [rootViewController presentViewController:konotorFeedbackScreen.conversationViewController animated:YES completion:^{
-         //   konotorFeedbackScreen.conversationViewController.view.layer.shouldRasterize = NO;
-         //   [KonotorFeedbackScreen refreshMessages];
-            if([[KonotorUIParameters sharedInstance] autoShowTextInput])
-                [konotorFeedbackScreen.conversationViewController performSelector:@selector(showTextInput) withObject:nil afterDelay:0.0];
-        }];
+            [konotorFeedbackScreen.conversationViewController setModalPresentationStyle:UIModalPresentationFullScreen];
+            [konotorFeedbackScreen.conversationViewController setModalTransitionStyle:UIModalTransitionStyleCrossDissolve];
+            
+            if([KonotorUtility KonotorIsInterfaceLandscape: (konotorFeedbackScreen.conversationViewController)]){
+                konotorFeedbackScreen.window=[[UIWindow alloc] initWithFrame:CGRectMake(0, 0, [UIScreen mainScreen].bounds.size.width, [UIScreen mainScreen].bounds.size.height)];
+            }
+            else{
+                konotorFeedbackScreen.window=[[UIWindow alloc] initWithFrame:[[[UIApplication sharedApplication] delegate] window].bounds];
+            }
+            
+            [konotorFeedbackScreen.window setBackgroundColor:[UIColor colorWithRed:0.2 green:0.2 blue:0.2 alpha:0.7]];
+            [konotorFeedbackScreen.window setRootViewController:konotorFeedbackScreen.conversationViewController];
+            
+            UIViewController *rootViewController=[[[[UIApplication sharedApplication] delegate] window] rootViewController];
+            if([rootViewController isKindOfClass:[UINavigationController class]])
+                rootViewController=[((UINavigationController*)rootViewController) topViewController];
+            UIViewController *presentedViewController=[rootViewController presentedViewController];
+            while(presentedViewController!=nil){
+                rootViewController=presentedViewController;
+                presentedViewController=[rootViewController presentedViewController];
+            }
+            
+            [rootViewController presentViewController:konotorFeedbackScreen.conversationViewController animated:YES completion:^{
+                //   konotorFeedbackScreen.conversationViewController.view.layer.shouldRasterize = NO;
+                //   [KonotorFeedbackScreen refreshMessages];
+                if([[KonotorUIParameters sharedInstance] autoShowTextInput])
+                    [konotorFeedbackScreen.conversationViewController performSelector:@selector(showTextInput) withObject:nil afterDelay:0.0];
+            }];
+        });
     }
     return YES;
 }
